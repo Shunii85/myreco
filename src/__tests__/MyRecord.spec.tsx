da385fd3943cf647dbc496e5f84c246650bba643
shunii <r05i36@ed.cc.suzuka-ct.ac.jp>
2024-03-02 17:39:37 +0900

    add a test about adding new record

--- src/__tests__/MyRecord.spec.tsx
+++ src/__tests__/MyRecord.spec.tsx
@@ -1,5 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { MyRecord } from "../MyRecord";
+import { Record } from "../domain/record";
 
 describe("title", () => {
   it("should render title", async () => {
@@ -7,6 +9,51 @@ describe("title", () => {
 
     await waitFor(() => {
       expect(screen.getByText("マイレコ")).toBeInTheDocument();
+    });
+  });
+
+  it("shuold add a new record", async () => {
+    render(<MyRecord />);
+
+    // addRecordsをモック化
+    const mockAddRecords = jest
+      .fn()
+      .mockReturnValue([new Record("1", "test title", 10)]);
+    jest.mock("../utils/supabaseFunctions", () => {
+      return {
+        addRecords: mockAddRecords,
+      };
+    });
+
+    // 画面が表示されることを確認
+    await waitFor(() => {
+      expect(screen.getByText("マイレコ")).toBeInTheDocument();
+    });
+
+    // リストアイテムの数を取得
+    const recordsBefore = screen
+      .getByTestId("my-records")
+      .querySelectorAll("li").length;
+    console.log(recordsBefore);
+
+    // モーダルを開く
+    await userEvent.click(screen.getByTestId("open-modal"));
+
+    const titleInput = screen.getByTestId("title");
+    const timeInput = screen.getByTestId("time");
+    const submitButton = screen.getByTestId("submit");
+
+    // フォームの値を入力
+    await userEvent.type(titleInput, "test title");
+    await userEvent.type(timeInput, "10");
+    // フォームを送信
+    await userEvent.click(submitButton);
+
+    // リストが一つ増えていることを確認
+    await waitFor(() => {
+      expect(
+        screen.getByTestId("my-records").querySelectorAll("li").length
+      ).toBe(recordsBefore + 1);
     });
   });
 });
